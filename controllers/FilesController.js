14f1fc38cb1a3f6c587dcca817df742316ff020e
saad-out <outsaad03@gmail.com>
2023-09-06 11:28:02 +0100

    verify data

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,5 +1,8 @@
+import dbClient from '../utils/db';
 // eslint-disable-next-line import/named
 import { getUserByToken } from '../utils/auth';
+
+const acceptedTypes = ['folder', 'file', 'image'];
 
 class FilesController {
   static async postUpload(req, res) {
@@ -9,6 +12,14 @@ class FilesController {
       name, type, parentId = 0, isPublic = false, data,
     } = req.body;
     console.log(`name: ${name}, type: ${type}, parentId: ${parentId}, isPublic: ${isPublic}, data: ${data}`);
+    if (!name) return res.status(400).send({ error: 'Missing name' });
+    if (!type || !acceptedTypes.includes(type)) return res.status(400).send({ error: 'Missing type' });
+    if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
+    if (parentId !== 0) {
+      const parent = await dbClient.filterBy('files', { _id: parentId });
+      if (!parent) return res.status(400).send({ error: 'Parent not found' });
+      if (parent.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
+    }
 
     return res.status(200).send({ status: 'OK' });
   }
