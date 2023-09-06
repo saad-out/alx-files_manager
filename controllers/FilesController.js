0169d5662453f8e62d85ea49d0be810840c5548d
saad-out <outsaad03@gmail.com>
2023-09-06 11:19:15 +0100

    get req.body params

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,6 +5,11 @@ class FilesController {
   static async postUpload(req, res) {
     const { user } = await getUserByToken(req);
     if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    const {
+      name, type, parentId = 0, isPublic = false, data,
+    } = req.body;
+    console.log(`name: ${name}, type: ${type}, parentId: ${parentId}, isPublic: ${isPublic}, data: ${data}`);
+
     return res.status(200).send({ status: 'OK' });
   }
 }
