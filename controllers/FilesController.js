6b4e919f24f8df01eb7f2fd583dd09c5a77f869d
saad-out <outsaad03@gmail.com>
2023-09-06 11:44:31 +0100

    handle folders

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -12,6 +12,8 @@ class FilesController {
       name, type, parentId = 0, isPublic = false, data,
     } = req.body;
     console.log(`name: ${name}, type: ${type}, parentId: ${parentId}, isPublic: ${isPublic}, data: ${data}`);
+
+    // Validate parameters
     if (!name) return res.status(400).send({ error: 'Missing name' });
     if (!type || !acceptedTypes.includes(type)) return res.status(400).send({ error: 'Missing type' });
     if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
@@ -19,6 +21,21 @@ class FilesController {
       const parent = await dbClient.filterBy('files', { _id: parentId });
       if (!parent) return res.status(400).send({ error: 'Parent not found' });
       if (parent.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
+    }
+
+    // Create file
+    const file = {
+      userId: user._id,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+    if (type === 'folder') {
+      const newFolder = await dbClient.insertInto('files', file);
+      file.id = newFolder.insertedId;
+      delete file._id;
+      return res.status(201).send(file);
     }
 
     return res.status(200).send({ status: 'OK' });
